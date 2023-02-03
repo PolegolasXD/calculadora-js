92035c78db947d4f6b8556a2e929b20923daddcb
PolegolasXD <polegolasplays@gmail.com>
2023-02-03 19:50:30 -0300

    fix: melhorando o estilo da calc -colocando mais teclas -buscando valor de tecla com quarySelector

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const form = document.getElementById('form-principal')
 const inputUm = document.getElementById('input-1');
 const inputDois = document.getElementById('input-2');
 
-const buttonSum = document.querySelector('#sum-operation')
-const buttonSubtraction = document.querySelector('#subtraction-operation')
-const buttonMultiplication = document.querySelector('#multiplication-operation')
-const buttonDivision = document.querySelector('#division-operation')
-const buttonClear = document.querySelector('#clear-operation')
-const buttonEqual = document.querySelector('#equal-operation')
-
 let valorTotal = 0;
 
 function sum() {
@@ -43,12 +36,46 @@ function clear() {
 
 function equal() {
     console.log(valorTotal)
-
-    const element = document.getElementById('div-receber');
-    var text = document.createTextNode(`o valor da operação é ${valorTotal}`);
-    element.appendChild(text);
+    const element = document.getElementById('div-receber').innerHTML = `o valor da operação é ${valorTotal}`;
 }
 
+
+/*configurações botoes*/ 
+
+// const buttonUm = document.getElementById('number-1')
+// const buttonDois = document.getElementById('number-2')
+// const buttonTres = document.getElementById('number-3')
+// const buttonQuatro = document.getElementById('number-4')
+// const buttonCinco = document.getElementById('number-5')
+// const buttonSeis = document.getElementById('number-6')
+// const buttonSete = document.getElementById('number-7')
+// const buttonOito = document.getElementById('number-8')
+// const buttonNove = document.getElementById('number-9')
+// const buttonZero = document.getElementById('number-0')
+
+
+// function selectNumberUm(){
+//     inputUm.value = 1;
+// }
+// buttonUm.addEventListener('click', selectNumberUm);
+// buttonDois.addEventListener('click', selectNumberUm);
+// buttonTres.addEventListener('click', selectNumberUm);
+// buttonQuatro.addEventListener('click', selectNumberUm);
+// buttonUm.addEventListener('click', selectNumberUm);
+// buttonUm.addEventListener('click', selectNumberUm);
+// buttonUm.addEventListener('click', selectNumberUm);
+// buttonUm.addEventListener('click', selectNumberUm);
+// buttonUm.addEventListener('click', selectNumberUm);
+// buttonUm.addEventListener('click', selectNumberUm);
+
+
+/*-------------------------------------------------------------------------------------------------------------*/
+const buttonSum = document.querySelector('#sum-operation')
+const buttonSubtraction = document.querySelector('#subtraction-operation')
+const buttonMultiplication = document.querySelector('#multiplication-operation')
+const buttonDivision = document.querySelector('#division-operation')
+const buttonClear = document.querySelector('#clear-operation')
+const buttonEqual = document.querySelector('#equal-operation')
 
 buttonSum.addEventListener('click', sum);
 buttonSubtraction.addEventListener('click', subtraction);
